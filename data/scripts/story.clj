1a06129967e5816fc678897f68e192cd3f068fb0
caryoscelus <caryoscelus@gmx.com>
2013-05-11 01:11:23 +0400

    a bit of story

--- data/scripts/story.clj
+++ data/scripts/story.clj
@@ -3,6 +3,8 @@
 ;; bind events (bind id name action)
 ;; id is for map bindings, otherwise unrequired (pass -1 for it)
 ;; name should be unique and can be used to call this event manually
+;; THIS IS EXAMPLE
+(comment
 (bind 0 "Hello" (txt "Hello, world"))
 (bind 1 "Clojure" (txt "Hello from Clojure"))
 (bind 2 "StoryDialog"
@@ -20,8 +22,24 @@
 "Big chunk of text
 Really big
 And even bigger!!!
-Some very long string to check wrapping.. Hopefully it won't be borked"))
+Some very long string to check wrapping.. Hopefully it won't be borked")))
 
+
+
+(bind 0 "Oasis-1" (dia
+"It's good to run into oasis after long jorney in the desert.
+I drinked some water and filled bottles. Ahh.. so refreshing!"
+{
+ "Go futher! I have no more time to spend here."
+    (txt "I'd want to stay here for longer.. But i need to go"),
+ "Stay a little bit more.."
+    (txt "I can relax a bit more.. I lean on palm. Huh.. Something crunched under me.
+When i looked back, i discovered bunch of old bones. Human bones.
+All my relaxation disappeared right away..")
+}))
+
+(bind 1 "Broken house" (txt
+"I found abandoned house. "))
 
 
 ;; help
@@ -34,6 +52,7 @@ Controls:
 [arrows] - moving
 [space]  - activate feature
 [f1]     - call this help
+[f2]     - switch sound
 
 Good luck!"))
 
