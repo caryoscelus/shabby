c0430f247ccf201ff321c5ce88cf565ae7223021
caryoscelus <caryoscelus@gmx.com>
2013-08-28 13:49:26 +0400

    maps: move tilesets with properties out

---
+++ data/maps/gameplay-tiles.tsx
@@ -0,0 +1,39 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="gameplay-tiles" tilewidth="16" tileheight="16">
+ <image source="gameplay-tiles.png" width="800" height="600"/>
+ <tile id="0">
+  <properties>
+   <property name="status" value="empty"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="status" value="unpassable"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="status" value="road"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="status" value="enter"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="status" value="oasis"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="status" value="spawn"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="status" value="mapborder"/>
+  </properties>
+ </tile>
+</tileset>
---
+++ data/maps/quest-tiles.tsx
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="quest-tiles" tilewidth="16" tileheight="16">
+ <image source="quest-tiles.png" width="96" height="16"/>
+ <tile id="0">
+  <properties>
+   <property name="name" value="oasis"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="name" value="broken"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="name" value="oldman"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="name" value="bar"/>
+   <property name="auto" value="true"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="auto" value="true"/>
+   <property name="name" value="bar-out"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="clickable" value="1"/>
+   <property name="name" value="barman"/>
+  </properties>
+ </tile>
+</tileset>
